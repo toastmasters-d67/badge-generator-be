695362a34870bc69784d6c54908377ae87a50dfc
Sherry Lee <sherrymta520@gmail.com>
2024-03-25 20:45:14 +0800

    Add print statements to main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from typing import Union
 
 from fastapi import FastAPI
 
+
 app = FastAPI()
 
 
@@ -13,3 +14,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+print('Sherry')
+print('Sherry')
